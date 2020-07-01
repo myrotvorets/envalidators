91307fe28713f8291ce527f036696e33098b34a6
Myrotvorets <support@myrotvorets.center>
2020-07-01 19:12:46 +0300

    Add positiveInteger validator

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { ipList, ipListEx } from './ipList';
 export type { IPv4, IPv6 } from './ipList';
 export { iso31661Alpha2, iso31661Alpha2List } from './iso31661Alpha2';
 export { portOrZero } from './portOrZero';
+export { positiveInteger } from './positiveInteger';
---
+++ src/test/positiveInteger.test.ts
@@ -0,0 +1,20 @@
+import { EnvError } from 'envalid';
+import { positiveInteger } from '../';
+
+const validator = positiveInteger();
+
+describe('positiveInteger', (): void => {
+    it.each([['bad'], ['123.0'], ['-1'], ['false'], ['null'], ['0'], ['']])(
+        'should reject invalid input (%s)',
+        (input: string) => {
+            expect(() => validator._parse(input)).toThrowError(EnvError);
+        },
+    );
+
+    it.each([['1'], ['100'], ['1024'], ['60000'], ['65535'], ['1048576']])(
+        'should accept positive integers (%s)',
+        (input: string): void => {
+            expect(validator._parse(input)).toEqual(+input);
+        },
+    );
+});
