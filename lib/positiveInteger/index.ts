80bc2d40662990647d09889e0bc0ee041293d24d
Myrotvorets <support@myrotvorets.center>
2023-12-27 07:59:09 +0200

    Remove useless template literals

--- lib/positiveInteger/index.ts
+++ lib/positiveInteger/index.ts
@@ -2,7 +2,7 @@ import { EnvError, makeValidator } from 'envalid';
 
 export const positiveInteger = makeValidator((input: string) => {
     const coerced = +input;
-    if (Number.isNaN(coerced) || `${coerced}` !== `${input}` || coerced % 1 !== 0 || coerced <= 0) {
+    if (Number.isNaN(coerced) || `${coerced}` !== input || coerced % 1 !== 0 || coerced <= 0) {
         throw new EnvError(`Invalid port input: "${input}"`);
     }
 
